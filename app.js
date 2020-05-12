085410946b0bf6b28297c98df4d520de344a89b9
Kamrul Islam <rrhridoy1000@gmail.com>
2020-05-12 23:36:02 +0600

    update previous commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //jshint esversion: 6
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+//const bodyParser = require("body-parser");
+// const request = require("request");
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-//app.use(express.urlencoded({extended: false}));
+//app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: false}));
 
 
 
